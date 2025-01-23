2dfc27889bf3aba04f505fcba09bea7e23dddcc9
cmc228 <cmc228@bath.ac.uk>
2025-01-23 22:12:28 +0000

    modified config file to add the new variables used : key and url

--- config.py
+++ config.py
@@ -4,3 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()  # Loads the environment variables from the .env file
 
 DATABASE_URI = os.getenv("DATABASE_URI")
+API_KEY = os.getenv("API_KEY")
+API_URL = os.getenv("API_URL")
+
